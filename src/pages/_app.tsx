b3659c309f178b3504f9347d76d157a7ae5976d5
Ploy <117616170+yaminP@users.noreply.github.com>
2023-07-01 14:32:14 +0700

    app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>TW-Test</title>
+          <title>Me Prompt</title>
         </Head>
           <Component {...pageProps} />
       </React.Fragment>
