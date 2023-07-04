2bce101d2a7a60835ebe521090c1a118cafb3d5c
devilzeros <devilzeros@outlook.co.th>
2023-07-04 09:05:44 +0700

    est

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import Head from "next/head";
 import type { AppProps } from 'next/app'
-import { SSRProvider } from "react-bootstrap";
 import '../scss/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <SSRProvider>
-      <React.Fragment>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>Me Prompt</title>
-        </Head>
-          <Component {...pageProps} />
-      </React.Fragment>
-    </SSRProvider >
+    <React.Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>Me Prompt</title>
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
   )
 }
