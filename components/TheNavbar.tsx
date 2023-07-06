b029e501e230cdd0a3ecd17ff92ec7189d91472d
devilzeros <devilzeros@outlook.co.th>
2023-07-06 20:29:32 +0700

    est

--- components/TheNavbar.tsx
+++ components/TheNavbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import {
   Navbar,
   MobileNav,
@@ -7,71 +7,132 @@ import {
   IconButton,
   Card,
 } from "@material-tailwind/react";
+import { Menu, Transition } from "@headlessui/react";
 
+import { AiOutlineDown, AiFillHeart, AiOutlineUp } from "react-icons/ai";
+import { FiAlignJustify } from "react-icons/fi";
+import { RxCross1 } from "react-icons/rx";
+import Link from 'next/link'
 
 const TheNavbar: React.FC = () => {
-  // const [openNav, setOpenNav] = React.useState<boolean>(false);
+  const [openNav, setOpenNav] = useState<boolean>(false);
+  const [navOpen, setNav] = useState<boolean>(true)
 
-  // React.useEffect(() => {
-  //   window.addEventListener(
-  //     "resize",
-  //     () => window.innerWidth >= 960 && setOpenNav(false)
-  //   );
-  // }, []);
+  const handleNav = () => {
+    setNav(!navOpen)
+  }
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
 
-  // const navList = (
-  //   <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-  //     <Typography
-  //       as="li"
-  //       variant="small"
-  //       color="blue-gray"
-  //       className="p-1 font-normal"
-  //     >
-  //       <a href="#" className="flex items-center">
-  //         Pages
-  //       </a>
-  //     </Typography>
-  //     <Typography
-  //       as="li"
-  //       variant="small"
-  //       color="blue-gray"
-  //       className="p-1 font-normal"
-  //     >
-  //       <a href="#" className="flex items-center">
-  //         Account
-  //       </a>
-  //     </Typography>
-  //     <Typography
-  //       as="li"
-  //       variant="small"
-  //       color="blue-gray"
-  //       className="p-1 font-normal"
-  //     >
-  //       <a href="#" className="flex items-center">
-  //         Blocks
-  //       </a>
-  //     </Typography>
-  //     <Typography
-  //       as="li"
-  //       variant="small"
-  //       color="blue-gray"
-  //       className="p-1 font-normal"
-  //     >
-  //       <a href="#" className="flex items-center">
-  //         Docs
-  //       </a>
-  //     </Typography>
-  //   </ul>
-  // );
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Pages
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Blocks
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Menu as="div" className="relative text-left">
+          <div onClick={handleNav}>
+            <Menu.Button className="inline-flex rounded-md text-black hover:text-violet-700">
+              About
+              {!navOpen ?
+                <AiOutlineDown className="ml-2 -mr-1 h-5 w-3 text-gray-600 hover:text-violet-700"
+                  aria-hidden="true" />
+                :
+                <AiOutlineUp
+                  className="ml-2 -mr-1 h-5 w-3 text-gray-600 hover:text-violet-700"
+                  aria-hidden="true"
+                />}
+
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+
+                  {({ active }) => (
+                    <a href="about">
+                      <button
+                        className={`${active ? 'bg-sky-300 text-white ' : 'text-gray-900'
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        เกี่ยวกับเรา
+                      </button>
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a href="teams">
+                      <button
+                        className={`${active ? 'bg-violet-400 text-white' : 'text-gray-900'
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Team
+                      </button>
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </Typography>
+    </ul>
+  );
   return (
     <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
-      {/* <div className="flex items-center justify-between text-blue-gray-900">
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
-          href="#"
+          href="/"
           className="mr-4 cursor-pointer py-1.5 font-medium"
         >
-          Material Tailwind
+          <img src="./images/new-logo.png" className=" h-14 mr-3" alt="" />
         </Typography>
         <div className="flex items-center gap-4">
           <div className="mr-4 hidden lg:block">{navList}</div>
@@ -126,7 +187,7 @@ const TheNavbar: React.FC = () => {
         <Button variant="gradient" size="sm" fullWidth className="mb-2">
           <span>Buy Now</span>
         </Button>
-      </MobileNav> */}
+      </MobileNav>
     </Navbar>
   )
 }
