4d4664ef6129d8efe74df807c65f9ac7aa137f82
devilzeros <devilzeros@outlook.co.th>
2023-07-06 20:05:30 +0700

    est

--- components/TheNavbarcopy.tsx
+++ components/TheNavbarcopy.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Navbar,
   MobileNav,
@@ -7,17 +7,17 @@ import {
   IconButton,
   Card,
 } from "@material-tailwind/react";
- 
+
 export default function TheNavbarTwo() {
-  const [openNav, setOpenNav] = React.useState(false);
- 
-  React.useEffect(() => {
+  const [openNav, setOpenNav] = useState<boolean>(false);
+
+  useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
- 
+
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -62,7 +62,7 @@ export default function TheNavbarTwo() {
       </Typography>
     </ul>
   );
- 
+
   return (
     <>
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = withMT({
+
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
@@ -12,11 +16,11 @@ module.exports = withMT({
   theme: {
     extend: {
       fontFamily: {
-        font01: "'Prompt', sans-serif",
-      },
+        font01: "'Prompt', sans-serif"
+      }
     },
   },
   plugins: [
-    require('tailwindcss-animated'),
+    require('tailwindcss-animated')
   ],
 });
