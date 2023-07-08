03e34f6f6d867f1266024fddec92645f892a5662
devilzeros <devilzeros@outlook.co.th>
2023-07-08 18:08:21 +0700

    est

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,18 @@ import type { AppProps } from 'next/app'
 import '../scss/globals.scss'
 import { LanguageContext } from '@/components/Language/LanguageContext';
 import { Language } from '@/components/Language/types';
+import { ParallaxProvider } from 'react-scroll-parallax';
+
+
 export default function App({ Component, pageProps }: AppProps) {
   const [currentLanguage, setCurrentLanguage] = useState<Language>('TH');
   return (
     <React.Fragment>
+
       <LanguageContext.Provider value={{ currentLanguage, setCurrentLanguage }}>
-        <Component {...pageProps} />
+        <ParallaxProvider>
+          <Component {...pageProps} />
+        </ParallaxProvider>
       </LanguageContext.Provider>
     </React.Fragment>
   )
