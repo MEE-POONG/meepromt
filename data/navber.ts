66f9fe17b3745b1c61421728f3f18181b0cf1a47
devilzeros <devilzeros@outlook.co.th>
2023-07-08 10:59:27 +0700

    est

--- data/navber.ts
+++ data/navber.ts
@@ -1,18 +1,18 @@
 export const navData = [
-    { name: { EN: "home", TH: "หน้าแรก" }, pathLink: "/", headMenu: [] },
+    { name: { EN: "Home", TH: "หน้าแรก" }, pathLink: "/", headMenu: [] },
     {
-        name: { EN: "news", TH: "ข่าวสาร" }, pathLink: "/news", headMenu: [
-            { name: { EN: "activities", TH: "กิจกรรมองค์กร" }, pathLink: "/inter-activity", headLink: '/news' },
+        name: { EN: "News", TH: "ข่าวสาร" }, pathLink: "/news", headMenu: [
+            { name: { EN: "Activity", TH: "กิจกรรมองค์กร" }, pathLink: "/activity", headLink: '/news' },
         ]
     },
     {
-        name: { EN: "about us", TH: "เกี่ยวกับเรา" }, pathLink: "/about", headMenu: [
-            { name: { EN: "team", TH: "ทีมงาน" }, pathLink: "/teams", headLink: '/about' },
-            { name: { EN: "other services", TH: "บริการด้านอื่น" }, pathLink: "/other-services", headLink: '/about' },
+        name: { EN: "About US", TH: "เกี่ยวกับเรา" }, pathLink: "/about", headMenu: [
+            { name: { EN: "Team", TH: "ทีมงาน" }, pathLink: "/teams", headLink: '/about' },
+            { name: { EN: "Our Services", TH: "บริการด้านอื่น" }, pathLink: "/our-services", headLink: '/about' },
         ]
     },
-    { name: { EN: "our work", TH: "ผลงาน" }, pathLink: "/ourwork", headMenu: [] },
-    { name: { EN: "contact", TH: "ติดต่อเรา" }, pathLink: "/contact", headMenu: [] },
+    { name: { EN: "Our Work", TH: "ผลงาน" }, pathLink: "/ourwork", headMenu: [] },
+    { name: { EN: "Contact", TH: "ติดต่อเรา" }, pathLink: "/contact", headMenu: [] },
 ];
 // { name: { EN: "Home", TH: "หน้าแรก" }, pathLink: "/", headMenu: false },
 // { name: { EN: "News", TH: "ข่าวสาร" }, pathLink: "/news", headMenu: true },
