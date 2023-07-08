d3250ae39ad47e9100500630eb34a899312135fa
devilzeros <devilzeros@outlook.co.th>
2023-07-08 15:53:10 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import TheLayout from '@/components/TheLayout'
 import HometIndex from '@/container/Home_index'
 import GuildIndex from '@/container/Guild_index'
 import { BsFacebook, BsPhone, BsPhoneFill } from 'react-icons/bs'
+import HomeSlide from '@/container/HomeSlide'
 export default function Home() {
   return (
     <TheLayout>
@@ -13,24 +14,13 @@ export default function Home() {
         />
         <title>Me Prompt Technology</title>
       </Head>
-      <div className="container text-blue-400 mx-auto text-center mt-24 font-font01">
-        <div className="flex flex-col justify-center items-center text-center animate-fade-up">
-          <img src="images/new-logo.png" className="w-72 " />
-          <p className="max-w-sm font-bold text-xl mb-5">
-            Me Prompt Technology Company Limited
-          </p>
-          <h2 className='font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-            รับทำเว็บไซต์ และการตลาดออนไลน์
-          </h2>
-          <h5>
-            <BsFacebook className='inline mr-1' />meprompttecnology
-            <BsPhoneFill className='inline mr-1 ml-2' />06-444-82650
-            <BsFacebook className='inline mr-1 ml-2' />meprompttecnology
-          </h5>
-        </div>
-        <HometIndex />
-        {/* <GuildIndex /> */}
+      <div className="home-page">
+        <div className="container text-blue-400 mx-auto text-center mt-24 font-font01">
+          <HomeSlide />
+          <HometIndex />
+          {/* <GuildIndex /> */}
 
+        </div>
       </div>
     </TheLayout>
   )
