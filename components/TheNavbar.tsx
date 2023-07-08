bfac36030f90fda499b4084dba5bec356f857220
devilzeros <devilzeros@outlook.co.th>
2023-07-08 11:06:21 +0700

    est

--- components/TheNavbar.tsx
+++ components/TheNavbar.tsx
@@ -30,15 +30,15 @@ export default function TheNavbar() {
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       {navData.map((navItem, index) => (
         <React.Fragment key={index}>
-          {navItem.headMenu.length === 0 ? (
+          {navItem?.headMenu.length === 0 ? (
             <Typography
               as="li"
               variant="small"
               color="blue"
-              className={`p-1 text-lg font-medium font-font01 text-black hover:text-blue-400 ${router.pathname === navItem.pathLink ? "active:text-blue-400" : ""}`}
+              className={`p-1 text-lg font-medium font-font01 text-black hover:text-blue-400 ${router?.pathname === navItem?.pathLink ? "text-blue-400" : ""}`}
             >
-              <Link href={navItem.pathLink} className="flex items-center">
-                {navItem.name.TH}
+              <Link href={navItem?.pathLink} className="flex items-center">
+                {navItem?.name?.TH}
               </Link>
             </Typography>
           ) : (
@@ -48,21 +48,21 @@ export default function TheNavbar() {
                   as="li"
                   variant="small"
                   color="blue"
-                  className={`p-1 text-lg font-medium font-font01 text-black hover:text-blue-400 ${router.pathname === navItem.pathLink ? "active:text-blue-400" : ""}`}
+                  className={`p-1 text-lg font-medium font-font01 text-black hover:text-blue-400 ${router?.pathname === navItem?.pathLink ? "text-blue-400" : ""}`}
                 >
-                  <Link href={navItem.pathLink} className="flex items-center">
-                    {navItem.name.TH}
+                  <Link href={navItem?.pathLink} className="flex items-center">
+                    {navItem?.name?.TH}
                   </Link>
                 </Typography>
               </MenuHandler>
               <MenuList >
-                {navItem.headMenu.map((subItem, subIndex) => (
+                {navItem?.headMenu.map((subItem, subIndex) => (
                   <li key={subIndex}
                     role="menuitem"
-                    className={`block w-full cursor-pointer font-font01 font-medium select-none rounded-md px-3 pt-[9px] py-2 text-center transition-all hover:border-none hover:bg-blue-400 hover:text-white focus-visible:outline-none focus:border-none focus:bg-blue-400 focus:text-white active:border-none active:bg-blue-400 active:text-white ${router.pathname === subItem.pathLink ? "active:text-white active:bg-blue-400" : ""}`}
+                    className={`block w-full cursor-pointer font-font01 font-medium select-none rounded-md px-3 pt-[9px] py-2 text-center transition-all hover:border-none hover:bg-blue-400 hover:text-white focus-visible:outline-none focus:border-none focus:bg-blue-400 focus:text-white active:border-none active:bg-blue-400 active:text-white ${router?.pathname === subItem?.pathLink ? "text-white bg-blue-400" : ""}`}
                   >
                     <Link href={subItem.pathLink}>
-                      {subItem.name.TH}
+                      {subItem.name?.TH}
                     </Link>
                   </li>
                 ))}
