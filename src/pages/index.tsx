a966b00a2028fc4983c7e6f7498256674c450876
devilzeros <devilzeros@outlook.co.th>
2023-07-11 11:24:36 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import TheLayout from '@/components/TheLayout'
 import HomeMain from '@/container/Home/HomeMain'
+import HomeCategories from '@/container/Home/HomeCategories'
 export default function Home() {
   return (
     <TheLayout>
@@ -13,21 +14,8 @@ export default function Home() {
       </Head>
       <div className='home-page'>
         <HomeMain />
+        <HomeCategories />
       </div>
-      {/* <div className="container text-blue-400 mx-auto text-center mt-24 font-font01">
-        <div className="flex flex-col justify-center items-center text-center animate-fade-up">
-          <img src="images/new-logo.png" className="w-72 " />
-          <p className="max-w-sm font-bold text-xl mb-5">
-            Me Prompt Technology Company Limited
-          </p>
-          <h2 className='font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-            รับทำเว็บไซต์ และการตลาดออนไลน์
-          </h2>
-          <h5>
-          </h5>
-        </div>
-
-      </div> */}
     </TheLayout>
   )
 }
